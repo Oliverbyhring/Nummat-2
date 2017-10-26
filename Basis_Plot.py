e8a0fd8d87a5ab0f0fc9e31a690e5203d005ec0c
Even Myklebust <even.myklebust@gmail.com>
2017-10-26 15:36:16 +0200

    opprettet Basis_Plot og en masterfil

---
+++ Basis_Plot.py
@@ -0,0 +1,59 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import splipy as sp
+
+
+def Basis_Plot():
+    order_p = 4 #Fra oppgave: the order is p + 1, where p is the polynomial degree of the spline
+    # create a set of B-spline basis functions
+    basis1 = sp.BSplineBasis(order=order_p, knots=[0, 0, 0, 1, 2, 3, 4, 4, 4])
+    basis2 = sp.BSplineBasis(order=order_p, knots=[0, 0, 0, 1, 1, 2, 2, 3, 3, 4, 4, 4])
+    basis3 = sp.BSplineBasis(order=order_p, knots=[0, 0, 0, 0, 1, 2, 3, 4, 4, 4, 4])
+    basis4 = sp.BSplineBasis(order=order_p, knots=[0, 0, 0, 0, 1, 1, 2, 2, 3, 3, 4, 4, 4, 4])
+
+    # 150 uniformly spaced evaluation points on the domain (0,4)
+    t = np.linspace(0, 4, 150)
+
+    # evaluate *all* basis functions on *all* points t. The returned variable B is a matrix
+    B1 = basis1.evaluate(t)  # Bi.shape = (150,6), 150 visualization points, 6 basis functions
+    B2 = basis2.evaluate(t)
+    B3 = basis3.evaluate(t)
+    B4 = basis4.evaluate(t)
+
+    # plot the basis functions
+
+    plt.figure()
+
+    plt.subplot(221)
+    plt.title("[0, 0, 0, 1, 2, 3, 4, 4, 4]")
+    plt.plot(t, B1)
+    plt.axis('off')
+
+    plt.subplot(222)
+    plt.title("[0, 0, 0, 1, 1, 2, 2, 3, 3, 4, 4, 4]")
+    plt.plot(t, B2)
+    plt.axis('off')
+
+    plt.subplot(223)
+    plt.title("[0, 0, 0, 0, 1, 2, 3, 4, 4, 4, 4]")
+    plt.plot(t, B3)
+    plt.axis('off')
+
+    plt.subplot(224)
+    plt.title("[0, 0, 0, 0, 1, 1, 2, 2, 3, 3, 4, 4, 4, 4]")
+    plt.plot(t, B4)
+    plt.axis('off')
+    """
+    plt.plot(t, B1)
+    plt.figure()
+    plt.plot(t, B2)
+    plt.figure()
+    plt.plot(t, B3)
+    plt.figure()
+    plt.plot(t, B4)
+    """
+    plt.show()
+
+
+if __name__ == "__main__":
+    Basis_Plot()
---
+++ master2.py
@@ -0,0 +1,7 @@
+import Basis_Plot
+
+def main():
+    Basis_Plot.task1a()
+
+if __name__ == "__main__":
+    main()
