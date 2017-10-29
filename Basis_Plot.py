8d5e6a2d096dd1266a97438aa31ba12b822cf457
Even Myklebust <even.myklebust@gmail.com>
2017-10-29 13:23:24 +0100

    printing av N-matrise

--- Basis_Plot.py
+++ Basis_Plot.py
@@ -4,12 +4,13 @@ import splipy as sp
 
 
 def Basis_Plot():
-    order_p = 4 #Fra oppgave: the order is p + 1, where p is the polynomial degree of the spline
+    #Fra oppgave: the order is p + 1, where p is the polynomial degree of the spline
+    # p can maximally be number of repeated points + 1 ?
     # create a set of B-spline basis functions
-    basis1 = sp.BSplineBasis(order=order_p, knots=[0, 0, 0, 1, 2, 3, 4, 4, 4])
-    basis2 = sp.BSplineBasis(order=order_p, knots=[0, 0, 0, 1, 1, 2, 2, 3, 3, 4, 4, 4])
-    basis3 = sp.BSplineBasis(order=order_p, knots=[0, 0, 0, 0, 1, 2, 3, 4, 4, 4, 4])
-    basis4 = sp.BSplineBasis(order=order_p, knots=[0, 0, 0, 0, 1, 1, 2, 2, 3, 3, 4, 4, 4, 4])
+    basis1 = sp.BSplineBasis(order=3, knots=[0, 0, 0, 1, 2, 3, 4, 4, 4])
+    basis2 = sp.BSplineBasis(order=3, knots=[0, 0, 0, 1, 1, 2, 2, 3, 3, 4, 4, 4])
+    basis3 = sp.BSplineBasis(order=4, knots=[0, 0, 0, 0, 1, 2, 3, 4, 4, 4, 4])
+    basis4 = sp.BSplineBasis(order=4, knots=[0, 0, 0, 0, 1, 1, 2, 2, 3, 3, 4, 4, 4, 4])
 
     # 150 uniformly spaced evaluation points on the domain (0,4)
     t = np.linspace(0, 4, 150)
@@ -19,7 +20,8 @@ def Basis_Plot():
     B2 = basis2.evaluate(t)
     B3 = basis3.evaluate(t)
     B4 = basis4.evaluate(t)
-
+    np.set_printoptions(suppress=True, linewidth=np.nan, threshold=np.nan)
+    print(B1)
     # plot the basis functions
 
     plt.figure()
--- master2.py
+++ master2.py
@@ -1,4 +1,11 @@
 import Basis_Plot
+import xml.etree.ElementTree as et
+import sys
+from math import*
+import splipy as sp
+import numpy as np
+import matplotlib as mpl
+import matplotlib.pyplot as plt
 
 def main():
     Basis_Plot.task1a()
