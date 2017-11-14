f6db74b0d9e849e99fc2cc6ae5cab312ed5293ae
sivertsel <18726460+sivertsel@users.noreply.github.com>
2017-11-14 13:58:29 +0100

    area_integral all right

--- Area_Integral.py
+++ Area_Integral.py
@@ -13,28 +13,28 @@ def Area_Integral():
     with G2('Area.g2') as my_file:
         my_Area = my_file.read()
 
-    # Get the surface object from my_Area
+    # Get the surface object, and fetch two knot vectors / two degrees
 
     surface = my_Area[0]   # create the NURBS surface
-    T = surface.knots(0, True) # get knots from tuple object
-    p = surface.order(0) - 1 # get degree (order - 1) from tuble object
+    T1 = surface.knots(0, True) # get knots from tuple object
+    T2 = surface.knots(1, True)
+    p1 = surface.order(0) - 1 # get degree (order - 1) from tuble object
+    p2 = surface.order(1) - 1
 
+    # Compute two sets of weights and nodes (2D-surface)
 
-    # Compute the weights and nodes from the knot vector with order p
+    w1, ksi1 = sq.Spline_Quadrature(T1,p1)
+    w2, ksi2 = sq.Spline_Quadrature(T2,p2)
 
-    w, ksi = sq.Spline_Quadrature(T,p)
-
-    # Evaluate the surface at the nodes to obtain du, dv
-
-    derivative = surface.derivative(ksi)
-    du = derivative[:,0]
-    dv = derivative[:,1]
-
-    # Compute the surface integral using the J(du,dv) and the computed weights
+    # Compute the surface integral using the |J(du,dv)| and the computed weights
 
     A = 0 # surface integral
-    for i in range(len(ksi)):
-        A += w[i]*np.cross(du[i],dv[i])
+
+    for i in range(len(ksi1)):
+        for j in range(len(ksi2)):
+            du = surface.derivative(ksi1[i],ksi2[j],d=(1,0))
+            dv = surface.derivative(ksi1[i],ksi2[j],d=(0,1))
+            A += w1[i] * w2[j] * abs(np.cross(du,dv))
 
     return A
 
