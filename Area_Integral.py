1e3df04045343c029c472618e4d1cb623bbdf81b
sivertsel <18726460+sivertsel@users.noreply.github.com>
2017-11-14 13:36:24 +0100

    area_integral spytter ut tall

--- Area_Integral.py
+++ Area_Integral.py
@@ -1,33 +1,43 @@
 from splipy.IO import * # "OI" changed to "io" in last edit - I dont have last edit
 import matplotlib.pyplot as plt
 import numpy as np
+import splipy as spl
+import scipy as sp
+import sys
 import Spline_Quadrature as sq
-#import splipy as spl
-#import scipy as sp
-#import sys
 
 def Area_Integral():
-    # Read a single NURBS patch from the file 'Curve.g2' NB: this file must be in the same directory
+
+    # Read surface file and store
 
     with G2('Area.g2') as my_file:
         my_Area = my_file.read()
 
+    # Get the surface object from my_Area
+
     surface = my_Area[0]   # create the NURBS surface
-    T = surface.knots(0, True) # få ut skjøtene fra tuple-objekt
-    p = surface.order(0) - 1 # få ut ordenen fra tuple-objekt
+    T = surface.knots(0, True) # get knots from tuple object
+    p = surface.order(0) - 1 # get degree (order - 1) from tuble object
+
+
+    # Compute the weights and nodes from the knot vector with order p
 
     w, ksi = sq.Spline_Quadrature(T,p)
+
+    # Evaluate the surface at the nodes to obtain du, dv
 
     derivative = surface.derivative(ksi)
     du = derivative[:,0]
     dv = derivative[:,1]
 
+    # Compute the surface integral using the J(du,dv) and the computed weights
+
     A = 0 # surface integral
     for i in range(len(ksi)):
         A += w[i]*np.cross(du[i],dv[i])
-    print(A)
+
     return A
 
 
 if __name__ == "__main__":
-    Area_Integral()
+    print(Area_Integral())
--- Curve_Integral.py
+++ Curve_Integral.py
@@ -1,34 +1,41 @@
 from splipy.IO import * # "OI" changed to "io" in last edit - I dont have last edit
 import matplotlib.pyplot as plt
 import numpy as np
+import splipy as spl
+import scipy as sp
+import sys
 import Spline_Quadrature as sq
-#import splipy as spl
-#import scipy as sp
-#import sys
 
 def Curve_Integral():
-    # Read a single NURBS patch from the file 'Curve.g2' NB: this file must be in the same directory
+
+    # Read a single NURBS patch from the file 'Curve.g2'
 
     with G2('Curve.g2') as my_file:
         my_curve = my_file.read()
 
-    curve = my_curve[0]   # create the NURBS curve
-    T = curve.knots(0, True) # få ut lista fra tuple-objekt
-    p = curve.order(0) - 1 # få ut ordenen fra tuple-objekt
+    # Create the NURBS curve, and get knot vector and order from curve object
+
+    curve = my_curve[0]
+    T = curve.knots(0, True)
+    p = curve.order(0) - 1 # degree = order - 1
+
+    # Compute the weights and nodes from the objects knot vector and degree
 
     w, ksi = sq.Spline_Quadrature(T,p)
-    print(ksi)
 
     I = 0 # line integral
     E = np.zeros(len(ksi))
 
 
+    # Compute the line integral: each step is weight * (du^2+dv^2) ^ 0.5
+
     for i in range(len(ksi)):
-        E = curve.derivative(ksi[i])
+        E = curve.derivative(ksi[i]) # evaluate the derivative in node i
         I += w[i]*np.sqrt(E[0]**2+E[1]**2)
-    print(I)
+
+
     return I
 
 
 if __name__ == "__main__":
-    Curve_Integral()
+    print(Curve_Integral())
